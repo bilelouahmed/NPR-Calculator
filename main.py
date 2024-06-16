ff78e29f82eb7ca354af698fd52f2f981b14bb4d
Bilel Ouahmed <bilel.ouahmed1@gmail.com>
2024-06-16 19:35:28 +0200

    Change API framework (from Flask to FastAPI)

--- main.py
+++ main.py
@@ -1,41 +1,48 @@
 import math
+
 import argparse
 
-from flask import Flask, request, jsonify
-from werkzeug.exceptions import BadRequest
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import uvicorn
 
-app = Flask(__name__)
+
+app = FastAPI()
 
 
-def calculator(expression: str, verbose=False) -> tuple["float", "str"]:
+class ExpressionRequest(BaseModel):
+    expression: str
+
+
+def calculator(expression: str, verbose=False) -> tuple[float, str]:
     fifo = []
     classic_exp_fifo = []
 
     tokens = expression.split(" ")
 
-    if tokens == [""]:
-        raise BadRequest("Error : The expression is empty.")
-
     for token in tokens:
+        if token == "":
+            continue
 
         if token.isdigit() or (token[0] == "-" and token[1:].isdigit()):
             fifo.append(float(token))
             classic_exp_fifo.append(token)
 
             if verbose:
-                print(f"Push : {token}\t---> Pile : {fifo}")
+                print(f"Push: {token}\t---> Pile: {fifo}")
 
         elif token in ["sin", "cos", "tan", "exp"]:
             if len(fifo) < 1:
-                raise BadRequest(
-                    "Error : The stack should contain at least one operand before a unary operator."
+                raise HTTPException(
+                    status_code=400,
+                    detail="Bad Request : The stack should contain at least one operand before a unary operator.",
                 )
 
             a = fifo.pop()
             first_operand = classic_exp_fifo.pop()
 
             if verbose:
-                print(f"Pop : {a} for operator '{token}'")
+                print(f"Pop: {a} for operator '{token}'")
 
             if token == "sin":
                 result = math.sin(a)
@@ -51,13 +58,14 @@ def calculator(expression: str, verbose=False) -> tuple["float", "str"]:
 
             if verbose:
                 print(
-                    f"Résultat de {token}({first_operand}) = {result}\t---> Pile : {fifo}"
+                    f"Résultat de {token}({first_operand}) = {result}\t---> Pile: {fifo}"
                 )
 
         else:
             if len(fifo) < 2:
-                raise BadRequest(
-                    "Error : The stack should contain at least two operands before a binary operator."
+                raise HTTPException(
+                    status_code=400,
+                    detail="Bad Request : The stack should contain at least two operands before a binary operator.",
                 )
 
             b = fifo.pop()
@@ -66,7 +74,7 @@ def calculator(expression: str, verbose=False) -> tuple["float", "str"]:
             first_operand = classic_exp_fifo.pop()
 
             if verbose:
-                print(f"Pop : {a} et {b} for operator '{token}'")
+                print(f"Pop: {a} et {b} for operator '{token}'")
 
             if token == "+":
                 result = a + b
@@ -76,24 +84,28 @@ def calculator(expression: str, verbose=False) -> tuple["float", "str"]:
                 result = a * b
             elif token == "/":
                 if b == 0:
-                    raise BadRequest("Error : Division per zero.")
+                    raise HTTPException(
+                        status_code=400, detail="Bad Request : Division per zero."
+                    )
                 result = a / b
             elif token == "^":
                 result = a**b
             else:
-                raise BadRequest(
-                    f"Error : Unknown operator '{token}'. Please, check the README file."
+                raise HTTPException(
+                    status_code=400,
+                    detail=f"Bad Request : Unknown operator '{token}'. Please, check the README file.",
                 )
 
             fifo.append(result)
             classic_exp_fifo.append(f"({first_operand} {token} {second_operand})")
 
             if verbose:
-                print(f"Result of {a} {token} {b} = {result}\t---> Pile : {fifo}")
+                print(f"Result of {a} {token} {b} = {result}\t---> Pile: {fifo}")
 
     if len(fifo) != 1:
-        raise BadRequest(
-            "Error : The expression is not valid. It should normally contain only one element in the stack at the end."
+        raise HTTPException(
+            status_code=400,
+            detail="Bad Request : The expression is not valid. It should normally contain only one element in the stack at the end.",
         )
 
     return fifo[0], classic_exp_fifo[0]
@@ -107,7 +119,7 @@ if __name__ == "__main__":
         "--verbose",
         type=bool,
         default=False,
-        help=f"Verbose mode. Default is False.",
+        help="Verbose mode. Default is False.",
     )
 
     args = parser.parse_args()
@@ -115,24 +127,23 @@ if __name__ == "__main__":
     VERBOSE = args.verbose
 
     @app.post("/calculator")
-    def main():
-        expression = request.json["expression"]
+    async def main(request: ExpressionRequest):
+        expression = request.expression
 
-        if not (expression):
-            raise BadRequest("Please, provide at least with field : expression.")
-
-        if not (type(expression) == str):
-            raise BadRequest(
-                "Please, check you typed well your fields (expression : str)."
+        if not expression:
+            raise HTTPException(
+                status_code=400,
+                detail="Bad Request : Please, provide at least the field: expression.",
             )
 
-        if set(request.json.keys()) - {"expression"}:
-            raise BadRequest(
-                "An extra field has been received. Please, check you provide your request with at most this argument : expression (str)."
+        if not isinstance(expression, str):
+            raise HTTPException(
+                status_code=400,
+                detail="Bad Request : Please, check you typed well your fields (expression: str).",
             )
 
         result, classic_expression = calculator(expression, verbose=VERBOSE)
 
-        return jsonify({"result": result, "expression": classic_expression})
+        return {"result": result, "expression": classic_expression}
 
-    app.run(host="0.0.0.0", port=6000)
+    uvicorn.run(app, host="0.0.0.0", port=6000)
